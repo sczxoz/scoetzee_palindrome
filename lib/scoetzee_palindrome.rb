77ee8cda1e46bcb747675350aacab9434f871b1b
Stef Coetzee <stefcoetzee@protonmail.com>
2020-07-26 16:16:26 +0200

    Finish working palindrome method

--- lib/scoetzee_palindrome.rb
+++ lib/scoetzee_palindrome.rb
@@ -1,6 +1,16 @@
 require "scoetzee_palindrome/version"
 
-module ScoetzeePalindrome
-  class Error < StandardError; end
-  # Your code goes here...
-end
+class String
+
+  # Returns true for a palindrome, false otherwise.
+  def palindrome?
+    processed_content == processed_content.reverse
+  end
+
+  private
+
+    # Returns content for palindrome testing.
+    def processed_content
+      self.split(/\W/).join.downcase
+    end
+end  
